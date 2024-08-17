0cbf293aa3efb0e8704bf46d86ef7bc85d886a57
marioskam3 <85133854+marioskam3@users.noreply.github.com>
2024-08-17 15:55:07 +0300

    connect supabase database

---
+++ server/database/dbconnect.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+from supabase import create_client
+
+url= os.environ.get("SUPABASE_URL")
+key= os.environ.get("SUPABASE_KEY")
+supabase = create_client(url, key)
+
+data = supabase.table("Users").select("*").execute()
+
+print(data.data)
+
+
